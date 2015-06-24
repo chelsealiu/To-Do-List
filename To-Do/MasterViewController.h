5e8e223d331c0a5fa5db2088abcaee29bfaf0995
Chelsea Liu <cl314@duke.edu>
2015-06-24 08:55:52 -0400

    Initial Commit

---
+++ To-Do/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  To-Do
+//
+//  Created by Chelsea Liu on 6/24/15.
+//  Copyright (c) 2015 Chelsea Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
